9faee9b931736cc1073d2df83f0b76926e6d25b8
Brian Burch <Bburch05@gmail.com>
2018-08-30 21:22:27 -0400

    "added API routes"

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,28 @@
-$(document).ready(function(){
-    $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
+$(document).ready(function() {
+  $(".navbar a, footer a[href='#myPage']").on("click", function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
           scrollTop: $(hash).offset().top
-        }, 900, function(){
-               window.location.hash = hash;
-        });s
-      } // End if
+        },
+        900,
+        function() {
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+
+  $(window).scroll(function() {
+    $(".slideanim").each(function() {
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+      if (pos < winTop + 600) {
+        $(this).addClass("slide");
+      }
     });
-    
-    $(window).scroll(function() {
-      $(".slideanim").each(function(){
-        var pos = $(this).offset().top;
-  
-        var winTop = $(window).scrollTop();
-          if (pos < winTop + 600) {
-            $(this).addClass("slide");
-          }
-      });
-    });
-  })
+  });
+});
