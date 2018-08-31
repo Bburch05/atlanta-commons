341197bc78da0944a58b58cbf35bff68b5ad6d46
yashK <yashkinariwalask95@gmail.com>
2018-08-30 21:22:00 -0400

    added some user inputs

--- models/post.js
+++ models/post.js
@@ -1,40 +1,38 @@
 module.exports = function(sequelize, DataTypes) {
-    var Post = sequelize.define("Post", {
-      title: {
-          type: DataTypes.STRING,
-          allowNull: false
-      },
-      text: {
-       type: DataTypes.TEXT,
-       allowNull: false
-      },
-      postType: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-            equals: ["issue","event"]
-          }
-      },
-      image: DataTypes.STRING,
-      address: DataTypes.STRING,
-      neighborhood: {
-          type: DataTypes.STRING,
-          allowNull: false
+  var Post = sequelize.define("Post", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    text: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    postType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        equals: ["issue", "event"]
+      }
+    },
+    image: DataTypes.STRING,
+    address: DataTypes.STRING,
+    neighborhood: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+
+  Post.associate = function(models) {
+    Post.belongsTo(models.Users, {
+      foreignKey: {
+        allowNull: false
       }
     });
 
-    Post.associate = function(models) {
-        
-        Post.belongsTo(models.Users,{
-            foreignKey:{
-                allowNull: false
-            }
-        });
-
-        Post.hasMany(models.Comments,{
-          onDelete: "cascade"
-        });
-      };
-    return Post;
+    Post.hasMany(models.Comments, {
+      onDelete: "cascade"
+    });
   };
-  
+  return Post;
+};
