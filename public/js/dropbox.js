71fcde7cfb7da9c4a2d4bd0f3f605ea2a594b123
Mogrady <mrogrady@att.net>
2018-09-01 16:05:58 -0400

    dropbox changes

--- public/js/dropbox.js
+++ public/js/dropbox.js
@@ -1,17 +1,33 @@
-var $submitBtn = $("#submit");
+//<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 
-var API = {
-    pushToDropBox: function(){
-    return $.ajax({
-      type: "POST",
-      url : 'https://content.dropboxapi.com/2/files/upload',
-      dataType: 'application/octet-stream',
-      headers : {
-        "Authorization" : "Bearer Token",
-        "Dropbox-API-Arg" : "{"path:"/newFolder/datasrc.js","mode":"add","autorename":false"}"
-            }
-        });
-    }
-}
+var CLOUDINARY_URL = "	https://api.cloudinary.com/v1_1/atlanta-commons/upload";
+var CLOUDINARY_UPLOAD_PRESET = "qie6uzuq";
+//Need to match up to the div with the User image tag
+var imgThumbnail = $("#userPic");
+//Need to match up with the picture upload input
+var imgUpload = $("#imgUploadInput");
 
-$submitBtn.on("click", pushToDropBox);
+imgUpload.on("click", function(event) {
+  var userPic = event.target.files[0];
+  var formData = new FormData();
+  formData.append("file", userPic);
+  formData.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
+
+  axios({
+    url: CLOUDINARY_URL,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    data: formData
+  })
+    .then(function(res) {
+      console.log(res);
+      imgThumbnail.src = res.data.secure_url;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+});
+
+//preset name qie6uzuq
