db02c22e3c1471a5659f249d41830db629be193c
yashK <yashkinariwalask95@gmail.com>
2018-09-02 09:05:59 -0400

    exaple

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -86,8 +86,12 @@ module.exports = function(app) {
       res.json(result);
     });
   });
+  app.get("/api/users", function(req, res) {
+    db.Users.findAll({}).then(function(result) {
+      res.json(result);
+    });
+  });
 
-  // Create a new example
   app.post("/api/posts", function(req, res) {
     db.Post.create(req.body).then(function(dbExample) {
       res.json(dbExample);
