0a9307825ddf60be201f2d666524c86dffb3a53b
Mogrady <mrogrady@att.net>
2018-09-05 20:02:21 -0400

    email function

--- public/js/dropbox.js
+++ public/js/dropbox.js
@@ -1,32 +1,33 @@
-var CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/atlanta-commons/upload";
-var CLOUDINARY_UPLOAD_PRESET = "qie6uzuq";
 //Need to match up to the div with the User image tag
-var imgThumbnail = $("#userPic");
-//Need to match up with the picture upload input
-var imgUpload = $("#profile-pic");
 
-$("#btn-signup").on("click", function(event) {
+$(document).on("change", "#profile-pic", function(event) {
   event.preventDefault();
-  var userPic = imgUpload.src();
-  var formData = new FormData();
-  formData.append("file", userPic);
-  formData.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
+  let cloudPreset = process.env.CLOUDINARY_UPLOAD_PRESET;
 
-  axios({
-    url: CLOUDINARY_URL,
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    data: formData
-  })
-    .then(function(res) {
-      console.log(res);
-      imgThumbnail.src = res.data.secure_url;
+  var imgUpload = event.target.files[0];
+  var formData = new FormData();
+  formData.append("file", imgUpload);
+  formData.append("upload_preset", cloudPreset);
+  console.log(formData);
+  $(document).on("click", "#btn-signup", function(event) {
+    event.preventDefault();
+    axios({
+      url: cloudURL,
+      method: "POST",
+      headers: {
+        baseURL: process.env.cloudURL,
+
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      data: formData
     })
-    .catch(function(err) {
-      console.log(err);
-    });
+      .then(function(res) {
+        var userPic = res.data.secure_url;
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  });
 });
 
 $(document).on("click", "#btn-login", function(e) {
@@ -37,10 +38,6 @@ $(document).on("click", "#btn-login", function(e) {
   var userPswd = $("#login-password")
     .val()
     .trim();
-  //   var User = {
-  //     name: userName,
-  //     password: userPswd
-  //   };
 
   $.get("/api/users/" + userName, function(data) {
     if (!data) {
@@ -50,3 +47,5 @@ $(document).on("click", "#btn-login", function(e) {
     }
   });
 });
+
+module.export = userPic;
