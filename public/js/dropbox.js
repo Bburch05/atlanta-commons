55887241e48469a0b48fd735c49dec1f759f6adb
Brian Burch <Bburch05@gmail.com>
2018-09-06 21:33:45 -0400

    fixed issues with passport not redirecting

--- public/js/dropbox.js
+++ public/js/dropbox.js
@@ -1,5 +1,5 @@
 //Need to match up to the div with the User image tag
-var cloudPreset = qie6uzuq;
+var cloudPreset = "qie6uzuq";
 var cloudURL = "https://api.cloudinary.com/v1_1/atlanta-commons/upload";
 
 $(document).on("change", "#profile-pic", function(event) {
@@ -23,28 +23,10 @@ $(document).on("change", "#profile-pic", function(event) {
     })
       .then(function(res) {
         var userPic = res.data.secure_url;
+        $("#profile-pic").text(userPic);
       })
       .catch(function(err) {
         console.log(err);
       });
   });
 });
-
-$(document).on("click", "#btn-login", function(e) {
-  e.preventDefault;
-  var userName = $("#login-username")
-    .val()
-    .trim();
-
-    console.log(userName);
-  var userPswd = $("#login-password")
-    .val()
-    .trim();
-
-  $.post("/signin", user, function(data, status) {
-    console.log(status);
-    console.log(data);
-  });
-});
-
-module.export = userPic;
